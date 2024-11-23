c49534eefc3b13e2be7e45ca8e649c4e8adc6c09
ShibanshL <shibansh.w@gmail.com>
2024-11-23 12:57:26 +0530

    updated vercel.json

--- src/app.js
+++ src/app.js
@@ -34,12 +34,7 @@ app.use(express.json());
 app.use(bodyParser.json({ limit: "200mb" }));
 
 
-app.use(cors(
-  {
-    origin:"http://localhost:5173/React_FormBuilder/",
-    methods:["GET","POST","PATCH","PUT","DELETE"]
-  }
-));
+app.use(cors());
 
 // const {
 //   login,
